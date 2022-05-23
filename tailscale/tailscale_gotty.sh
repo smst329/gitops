b4977dfac2b83c48269c2671cc7950b3f6ff7e8f
smst329 <68325129+smst329@users.noreply.github.com>
2022-05-22 22:32:49 -0400

    Update tailscale_gotty.sh

--- tailscale/tailscale_gotty.sh
+++ tailscale/tailscale_gotty.sh
@@ -6,4 +6,4 @@ export TAILSCALE_IP=$(tailscale ip|head -n 1|grep -E "^100\.[0-9]+\.[0-9]+\.[0-9
 while [[ -z "$TAILSCALE_IP" ]]; do sleep 1; export TAILSCALE_IP=$(tailscale ip|head -n 1|grep -E "^100\.[0-9]+\.[0-9]+\.[0-9]+$"); done
 
 export TAILSCALE_IP=$(tailscale ip|head -n 1|grep -E "^100\.[0-9]+\.[0-9]+\.[0-9]+$");
-while true; do if test -n "$TAILSCALE_IP"; then /root/go/bin/gotty --address $TAILSCALE_IP -w zsh; fi; sleep 3; export TAILSCALE_IP=$(tailscale ip|head -n 1|grep -E "^100\.[0-9]+\.[0-9]+\.[0-9]+$"); done
+while true; do if test -n "$TAILSCALE_IP"; then /root/go/bin/gotty --address $TAILSCALE_IP -w zsh || true; fi; sleep 3; export TAILSCALE_IP=$(tailscale ip|head -n 1|grep -E "^100\.[0-9]+\.[0-9]+\.[0-9]+$"); done
